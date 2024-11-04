fc9c1cf446acf53c03b0402adff632daf6b939d2
Vahe Babayan <vahebabayan.tt219-2@polytechnic.am>
2024-11-04 18:35:52 +0400

    Update HashFunction.h

--- HashSet/HashFunction.h
+++ HashSet/HashFunction.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <cmath>   
 
-const size_t DEFAULT_PRIME = 997;
+const size_t DEFAULT_PRIME = 1000000007;
 
 class HashFunction
 {
@@ -12,12 +14,13 @@ public:
 	HashFunction(const HashFunction&);
 	HashFunction(size_t prime);
 	void setPrime(size_t prime);
-	size_t RollingHash(const std::string& key, size_t tableSize) const;
-	size_t CityHash(const std::string& key, size_t tableSize) const;
-	size_t FNV1a(const std::string & key, size_t tableSize) const;
-	size_t MurmurHash(const std::string& key, size_t tableSize) const;
+	std::string RollingHash(const std::string& key) const;
+	std::string CityHash(const std::string& key) const;
+	std::string FNV1a(const std::string& key) const;
+	std::string MurmurHash(const std::string& key) const;
 	static size_t generateRandomPrime(size_t min, size_t max);
 	static bool isPrime(size_t num);
 private:
 	size_t Prime;
+	size_t hashLength = 8;
 };
