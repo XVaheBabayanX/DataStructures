717c07fa8e9b69581aaaf67a294c21957c0c08c8
Vahe Babayan <vahebabayan.tt219-2@polytechnic.am>
2024-11-04 18:37:51 +0400

    Update HashSetMain.cpp

--- HashSet/HashSetMain.cpp
+++ HashSet/HashSetMain.cpp
@@ -1,35 +1,8 @@
 #include "Hashset.h"
 #include "HashFunction.h"
-#include <iostream>
 #include <string>
 
 using namespace std;
-
-bool isDouble(const std::string& str) {
-    if (str.empty()) return false;
-
-    size_t startIndex = 0;
-    bool decimalPointFound = false;
-
-    if (str[0] == '-') {
-        if (str.length() == 1) return false;
-        startIndex = 1;
-    }
-
-    for (size_t i = startIndex; i < str.length(); i++) {
-        if (str[i] == '.') {
-            if (decimalPointFound || i == startIndex || i == str.length() - 1) {
-                return false;
-            }
-            decimalPointFound = true;
-        }
-        else if (!isdigit(str[i])) {
-            return false;
-        }
-    }
-    return true;
-}
-
 
 bool isNumber(const std::string& str) {
     for (char c : str) {
@@ -43,30 +16,20 @@ bool isNumber(const std::string& str) {
 void instructions() {
     cout << "Enter one of the following options:\n"
         << " 1 to insert\n"
-        << " 2 to change\n"
-        << " 3 to delete\n"
-        << " 4 to search\n"
-        << " 5 to visualize\n"
-        << " 6 to print\n"
-        << " 7 to get elements count\n"
-        << " 8 to check load factor and potentially resize the table\n"
-        << " 9 to end\n";
+        << " 2 to delete\n"
+        << " 3 to search\n"
+        << " 4 to visualize\n"
+        << " 5 to print\n"
+        << " 6 to get elements count\n"
+        << " 7 to balance\n"
+        << " 8 to end\n";
 }
 
-const size_t CHOICE = 9;
+const size_t CHOICE = 8;
 
 int main() {
-    size_t tableSize;
     size_t prime;
     std::string input;
-
-    cout << "Enter hash table size : ";
-    cin >> input;
-
-    while (!isNumber(input) || (tableSize = std::stoul(input)) < MINIMAL_TABLE_SIZE) {
-        cout << "Table size must be at least " << MINIMAL_TABLE_SIZE << ". Please enter a valid size : ";
-        cin >> input;
-    }
 
     cout << "Enter prime number for hash function : ";
     cin >> input;
@@ -77,11 +40,10 @@ int main() {
     }
 
     HashFunction hashFunc(prime);
-    HashSet hashSet(tableSize, hashFunc);
+    HashSet hashSet(hashFunc);
 
     int choice;
     string key, newkey;
-    double value;
 
     instructions();
     cout << "What do you want to do? ";
@@ -107,122 +69,103 @@ int main() {
             if (hashSet.Search(key))
             {
                 cout << "Key " << key << " already exists.\n";
-                cout << "Would you like to change it? (y/n): ";
-                cin >> input;
-                if (input == "y" || input == "Y")
-                {
-                    cout << "Enter value to change : ";
-                    cin >> input;
-                    while (!isDouble(input)) {
-                        cout << "Value must be a number. Please enter a valid value : ";
-                        cin >> input;
-                    }
-                    value = std::stod(input);
-                    if (hashSet.Change(key, value)) {
-                        cout << "Key " << key << " value successfully changed.\n";
-                    }
-                    else cout << "Failed to change key value" << key << ".\n";
-                }
                 break;
             }
-            cout << "Enter value : ";
-            cin >> input;
-            while (!isDouble(input)) {
-                cout << "Value must be a number. Please enter a valid value : ";
-                cin >> input;
-            }
-            value = std::stod(input);
-            if (hashSet.Insert(key, value)) {
+            if (hashSet.Insert(key)) {
                 cout << "Key " << key << " successfully inserted.\n";
             }
             else cout << "Failed to insert key " << key << ".\n";
             break;
         case 2:
-            cout << "Enter the key that needs to be changed : ";
-            cin >> key;
-            while (key.empty() || !(hashSet.Search(key))) {
-                cout << "Key cannot be empty or non-existent. Please enter a valid key : ";
+            if (hashSet.getElementsCount() == 0)
+            {
+                cout << "The HashSet is empty, cannot delete in it.\n";
+            }
+            else
+            {
+                cout << "Enter key to delete : ";
                 cin >> key;
+                while (key.empty()) {
+                    cout << "Key cannot be empty. Please enter a valid key : ";
+                    cin >> key;
+                }
+                if (hashSet.Delete(key)) {
+                    cout << "Key " << key << " successfully deleted.\n";
+                }
+                else {
+                    cout << "Key " << key << " not found.\n";
+                }
             }
-            cout << "Enter value to change : ";
-            cin >> input;
-            while (!isDouble(input)) {
-                cout << "Value must be a number. Please enter a valid value : ";
-                cin >> input;
-            }
-            value = std::stod(input);
-            if (hashSet.Change(key, value)) {
-                cout << "Key " << key << " value successfully changed.\n";
-            }
-            else cout << "Failed to change key value" << key << ".\n";
             break;
         case 3:
-            cout << "Enter key to delete : ";
-            cin >> key;
-            while (key.empty()) {
-                cout << "Key cannot be empty. Please enter a valid key : ";
+            if (hashSet.getElementsCount() == 0)
+            {
+                cout << "The HashSet is empty, cannot search in it.\n";
+            }
+            else
+            {
+                cout << "Enter key to search : ";
                 cin >> key;
-            }
-            if (hashSet.Delete(key)) {
-                cout << "Key " << key << " successfully deleted.\n";
-            }
-            else {
-                cout << "Key " << key << " not found.\n";
+                while (key.empty()) {
+                    cout << "Key cannot be empty. Please enter a valid key : ";
+                    cin >> key;
+                }
+                if (Node* foundNode = hashSet.Search(key)) {
+                    cout << "Found key: " << key << endl;
+                }
+                else {
+                    cout << "Key not found.\n";
+                }
             }
             break;
         case 4:
-            cout << "Enter key to search : ";
-            cin >> key;
-            while (key.empty()) {
-                cout << "Key cannot be empty. Please enter a valid key : ";
-                cin >> key;
+            if (hashSet.getElementsCount() == 0)
+            {
+                cout << "The HashSet is empty, cannot visualize it.\n";
             }
-            if (Node* foundNode = hashSet.Search(key)) {
-                cout << "Found key: " << key << " with value : " << foundNode->_data << endl;
-            }
-            else {
-                cout << "Key not found.\n";
+            else
+            {
+                cout << "Table visualization:\n";
+                hashSet.TableVisualisation();
             }
             break;
         case 5:
-             cout << "Table visualization:\n";
-             hashSet.TableVisualisation();
+            if (hashSet.getElementsCount() == 0)
+            {
+                cout << "The HashSet is empty, cannot print it.\n";
+            }
+            else
+            {
+                cout << "Table:\n";
+                hashSet.Print();
+            }
             break;
         case 6:
-            cout << "Table:\n";
-            hashSet.Print();
+            if (hashSet.getElementsCount() == 0) cout << "HashSet is empty.\n";
+            else cout << "HashSet have " << hashSet.getElementsCount() << " elements\n";
             break;
         case 7:
-            if (hashSet.getElementsCount() == 0) cout << "HashTable is empty.\n";
-            else cout << "Hash Table have " << hashSet.getElementsCount() << " elements\n";
-            break;
-        case 8:
-            cout << "The Load Factor : " << hashSet.getTableLoadFactor() << "%" << endl;
-            if (hashSet.getTableLoadFactor() >= LOAD_FACTOR)
+            if (hashSet.getElementsCount() == 0)
             {
-                cout << "Load factor exceeds " << LOAD_FACTOR <<". Would you like to resize the hash table? (y/n): ";
-                cin >> input;
-                if (input == "y" || input == "Y")
+                cout << "The HashSet is empty, cannot balance it.\n";
+            }
+            else
+            {
+                if (hashSet.isBalanced())
                 {
-                    hashSet.ReHash();
-                    cout << "The hash table has been resized to " << hashSet.getSize() << "\n";
+                    cout << "The HashSet is already balanced.\n";
                 }
+                else
+                {
+                    hashSet.Balance();
+                    cout << "The HashSet was balanced.\n";
+                }
+
             }
             break;
         default:
             cout << "Invalid choice. Try again.\n";
             break;
-        }
-        if (hashSet.getTableLoadFactor() == 100.0)
-        {
-            cout << "Load factor exceeds " << LOAD_FACTOR << ". Would you like to resize the hash table or exit? (resize/r/1 or exit/e/2): ";
-            cin >> input;
-            if (input == "1" || input == "r" || input == "resize")
-            {
-                hashSet.ReHash();
-                cout << "The hash table has been resized to " << hashSet.getSize() << "\n";
-            }
-            else break;
         }
         cout << "What do you want to do next? ";
     }
