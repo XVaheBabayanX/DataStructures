9fe1a943d7a7b95cf78b38d9db78828125146cc5
Vahe Babayan <vahebabayan.tt219-2@polytechnic.am>
2024-11-09 15:01:06 +0400

    Update DirectionalGraph.h

--- DirectionalGraph/DirectionalGraph.h
+++ DirectionalGraph/DirectionalGraph.h
@@ -9,19 +9,25 @@ public:
     DirectionalGraph(size_t vertices);
     ~DirectionalGraph();
 
-    void addEdge(size_t u, size_t v, int weight);
+    void addVertex();
+    void removeVertex(size_t v);
+    size_t getVerticesCount() const { return _vertices; }
+
+    void addEdge(size_t u, size_t v, double weight);
     bool removeEdge(size_t u, size_t v);
+    bool changeEdge(size_t u, size_t v, double newWeight);
     bool hasEdge(size_t u, size_t v) const;
+    double getWeight(size_t u, size_t v) const;
+    size_t getEdgesCount() const { return _edges; }
+
     void printGraph();
+    bool isEmpty() const { return _vertices == 0; }
+    bool isConnected() const { return _edges > 0; }
 
     bool detectCycle();
     bool detectCycleUtil(size_t v, std::vector<bool>& visited, std::vector<bool>& recursionStack);
-
-    void addVertex();
-    void removeVertex(size_t v);
-    size_t getVertexCount() const { return vertices; }
-
 private:
     std::vector<DoublyLinkedList> adjList;
-    size_t vertices;
+    size_t _vertices;
+    size_t _edges;
 };
